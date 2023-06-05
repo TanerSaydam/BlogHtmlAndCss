69cf1af8e631a5ab1a763700358b0b474e9817bd
excalıbur <excalbur@DESKTOP-1IG1768>
2023-06-05 19:46:32 +0300

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Youtuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
